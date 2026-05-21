80cdfe6e9eaf6a9af5d4ce5d36f729761e027976
jiangshuo <1677252190@qq.com>
2026-05-21 09:19:59 +0800

    打包设置

--- extern/build-helper.sh
+++ extern/build-helper.sh
@@ -1,1 +1,5 @@
 cmake -B build -S . -G "Visual Studio 16 2019" && cmake --build build --config Debug --target install && cmake --build build --config Release --target install
+
+cmake -B build -S .  -G "Visual Studio 17 2022"   -T v142  -DUNREAL_ENGINE_ROOT="D:/soft/UnrealEngine/UE_5.3" -DCMAKE_CXX_FLAGS="/MP /WX-" -DEZVCPKG_BASEDIR="D:/Deps/cesium-ezvcpkg" 
+
+cmake -B build -S . --fresh  -G "Visual Studio 17 2022"   -T v142  -DUNREAL_ENGINE_ROOT="D:/soft/UnrealEngine/UE_5.3" -DCMAKE_CXX_FLAGS="/MP /WX-" -DEZVCPKG_BASEDIR="D:/Deps/cesium-ezvcpkg" 
